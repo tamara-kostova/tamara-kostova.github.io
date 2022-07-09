a5be851d040295ae4bb1c4fc735ef4724ceb3326
Tamara <tamarakostovabt@gmail.com>
2022-07-09 17:07:00 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,19 @@
-/* MENU SHOW Y HIDDEN */
 const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.getElementById('nav-close');
-/* MENU SHOW */
+
 if(navToggle){
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
-/* MENU HIDDEN */
+
 if(navClose){
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
-/* REMOVE MENU MOBILE */
+
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
@@ -22,7 +21,7 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
-/*SCROLL SECTIONS ACTIVE LINK */
+
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll("nav .nav__menu a");
 window.addEventListener("scroll", () => {
@@ -41,19 +40,19 @@ window.addEventListener("scroll", () => {
         }
     });
 });
-/* CHANGE BACKGROUND HEADER */
+
 function scrollHeader(){
     const nav = document.getElementById('header')
     if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
-/* SHOW SCROLL UP */
+
 function scrollUp(){
     const scrollUp = document.getElementById('scroll-up');
     if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
-/* DARK LIGHT THEME */
+
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'uil-sunset'
